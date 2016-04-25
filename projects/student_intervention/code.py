6ede01efa889b38dca49f3fc31c1f521eca8cee1
Alexandre Landi <alexandre.landi@etu.upmc.fr>
2016-04-25 14:17:04 +0200

    Add code.py.
    2016-04-25  Alexandre Landi  <alexandre.landi@etu.upmc.fr>
    
    	*  code.py: Add file.

---
+++ projects/student_intervention/code.py
@@ -0,0 +1,9 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+
+# Read student data
+student_data = pd.read_csv("student-data.csv")
+print "Student data read successfully!"
+# Note: The last column 'passed' is the target/label, all other are feature columns
+
